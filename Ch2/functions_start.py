0efd0677d0a021df6fa7baf7e7a52025a7f437d8
Neeraj55#5 <thakurfazer55@gmail.com>
2017-07-11 19:59:32 +0530

    Define and call function

---
+++ Ch2/functions_start.py
@@ -0,0 +1,7 @@
+#
+# Example file for working with functions
+# (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
+#Define a function
+def func1():
+    print("I am a function")
+func1()
