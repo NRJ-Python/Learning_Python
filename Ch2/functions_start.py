deb44185e7e8cc6210eedf710867032be3e5cd31
Neeraj55#5 <thakurfazer55@gmail.com>
2017-07-11 20:06:20 +0530

    Print the value of function [Python treats functions as Objects]

--- Ch2/functions_start.py
+++ Ch2/functions_start.py
@@ -6,3 +6,4 @@ def func1():
     print("I am a function")
 func1()
 print (func1())#Invokes function and then gives output none too as there is no return value
+print (func1) #Prints the value of the function
