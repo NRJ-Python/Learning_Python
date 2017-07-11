ab0ada7d91c72dc8cd468891dc25283907f91be8
Neeraj55#5 <thakurfazer55@gmail.com>
2017-07-11 21:20:23 +0530

    If else statement

--- Ch2/conditionals_start.py
+++ Ch2/conditionals_start.py
@@ -3,9 +3,11 @@
 # (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
 
 def main():
-  x, y = 10, 100
+  x, y = 1000, 100
   if(x<y):
       st="x is less than y"
+  else:
+      st="x is greater than y"
   print(st)
 if __name__ == "__main__":
  main()
