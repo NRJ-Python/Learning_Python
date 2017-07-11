b94847b07364d6dc63fb58671be1a66f7848e651
Neeraj55#5 <thakurfazer55@gmail.com>
2017-07-11 21:44:38 +0530

    While Loop

---
+++ Ch2/loops_start.py
@@ -0,0 +1,13 @@
+#
+# Example file for working with loops
+# (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
+
+def main():
+  x = 0
+  #While Loop
+  while(x<5):
+      print(x)
+      x=x+1
+  
+if __name__ == "__main__":
+  main()
