1e9db6c9f7b5578a20cbbd506bbcc4c9a9bc6822
Neeraj55#5 <thakurfazer55@gmail.com>
2017-07-11 20:41:58 +0530

    Define and print a function that takes arguments

--- Ch2/functions_start.py
+++ Ch2/functions_start.py
@@ -7,3 +7,9 @@ def func1():
 func1()
 print (func1())#Invokes function and then gives output none too as there is no return value
 print (func1) #Prints the value of the function
+#Functions that take arguments
+def func2(arg1,arg2):
+    print(arg1," ",arg2)
+func2(10,20)
+print (func2(10,20)) #Still prints none too as there is no return value
+    
