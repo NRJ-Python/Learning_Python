cdb58146311bf8a16c473b831b693b47b760d273
Neeraj55#5 <thakurfazer55@gmail.com>
2017-07-11 21:03:27 +0530

    Functions with variable number of arguments

--- Ch2/functions_start.py
+++ Ch2/functions_start.py
@@ -25,3 +25,11 @@ def power(num,x=1):
 print(power(2))
 print(power(2,3))
 print(power(x=3,num=2))
+#Functions with variable number of arguments
+def multi_add(*args):
+    result=0;
+    for x in args:
+        result=result+x
+    return result
+print(multi_add(1,2,3,4,1))
+    
