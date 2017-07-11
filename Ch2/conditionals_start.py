aaa295f5a0528c5bdb904668d6abf0a668a43300
Neeraj55#5 <thakurfazer55@gmail.com>
2017-07-11 21:13:25 +0530

    If statement

---
+++ Ch2/conditionals_start.py
@@ -0,0 +1,11 @@
+#
+# Example file for working with conditional statements
+# (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
+
+def main():
+  x, y = 10, 100
+  if(x<y):
+      st="x is less than y"
+  print(st)
+if __name__ == "__main__":
+ main()
