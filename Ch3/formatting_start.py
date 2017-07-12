2195e2ed977ad8b1193546cf143ad37621ca12c5
Neeraj55#5 <thakurfazer55@gmail.com>
2017-07-12 19:29:11 +0530

    Print Locale date and time

--- Ch3/formatting_start.py
+++ Ch3/formatting_start.py
@@ -12,6 +12,10 @@ def main():
   
   # %y/%Y - Year, %a/%A - weekday, %b/%B - month, %d - day of month
   print(now.strftime(("%Y"))) #Full year with century
-  print(now.strftime("%a,%d,%B,%y"))#Abbreviated day, num, full month, abbreviated year
+  print(now.strftime("%a,%d,%B,%y"))#abbreviated day, num, full month, abbreviated year
+  #%c - locale's date and time, %x - locale's date, %X - locale's time
+  print(now.strftime("%c"))
+  print(now.strftime("%x"))
+  print(now.strftime("%X"))
 if __name__ == "__main__":
   main();
