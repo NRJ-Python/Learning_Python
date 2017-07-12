3160c1c9ea1b0df98ddc4ee5045ec7098961b87f
Neeraj55#5 <thakurfazer55@gmail.com>
2017-07-12 16:43:40 +0530

    Creating a inherited class

--- Ch2/classes_start.py
+++ Ch2/classes_start.py
@@ -7,11 +7,15 @@ class myclass():
         print("Hello")
     def method2(self,mystring): #self argument is automatic and points to the object itself
         print("HELLO"+mystring)
-      
+class anotherclass(myclass):
+    def method1(self):
+        myclass.method1(self)
+        print("This is another class")      
 def main():
     c=myclass()
     c.method1()
     c.method2("Neeraj")
-  
+    c2=anotherclass()
+    c2.method1()
 if __name__ == "__main__":
   main()
