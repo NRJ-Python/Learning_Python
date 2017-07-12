86af3c36c0f0f0d92f3a76d316e4cd64cd7fdaec
Neeraj55#5 <thakurfazer55@gmail.com>
2017-07-12 16:54:43 +0530

    Import date and time modules and Print today's date

---
+++ Ch3/dates_start.py
@@ -0,0 +1,16 @@
+#
+# Example file for working with date information
+# (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
+
+from datetime import date
+from datetime import time
+from datetime import datetime
+def main():
+#Date Objects
+#Get today's date from today() method from date class
+ today=date.today()
+ print("Today's date is :",today)
+  
+if __name__ == "__main__":
+  main();
+  
