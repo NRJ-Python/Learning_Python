1834c3c5accf14e7ea972ab6e608c1c763b16783
Neeraj55#5 <thakurfazer55@gmail.com>
2017-07-12 17:34:19 +0530

    Formatting : Full year with century

---
+++ Ch3/formatting_start.py
@@ -0,0 +1,16 @@
+#
+# Example file for formatting time and date output
+# (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
+
+from datetime import datetime
+
+def main():
+  # Times and dates can be formatted using a set of predefined string
+  # control codes 
+  now = datetime.now() # get the current date and time
+    #### Date Formatting ####
+  
+  # %y/%Y - Year, %a/%A - weekday, %b/%B - month, %d - day of month
+  print(now.strftime(("%Y"))) #Full year with century
+if __name__ == "__main__":
+  main();
